caec28dc195a6dc848f00c3974db6f24a4455e16
NecErylmz <82933645+NecErylmz@users.noreply.github.com>
2022-03-02 13:01:43 +0300

    Uzay Savasi oyunu
    hyper casual game

---
+++ Uzay Savasi deneme/Assets/Scripts/UIKontrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIKontrol : MonoBehaviour
+{
+    [SerializeField]
+    GameObject oyunAdiText = default;
+
+    [SerializeField]
+    GameObject oyunBittiText = default;
+
+    [SerializeField]
+    Text puanText = default;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oyunBittiText.gameObject.SetActive(false);
+        puanText.gameObject.SetActive(false);
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
